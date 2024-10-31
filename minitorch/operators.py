0e3a8a0539f34b0cc8b1bb0bfb3c6714c6fa0923
Igor Ryabkov <Igor_Ryabkov_1@mail.ru>
2024-10-31 12:12:11 +0300

    test

---
+++ minitorch/operators.py
@@ -0,0 +1,118 @@
+"""Collection of the core mathematical operators used throughout the code base."""
+
+import math
+
+# ## Task 0.1
+# from typing import Callable, Iterable
+
+#
+# Implementation of a prelude of elementary functions.
+
+# Mathematical functions:
+# - mul
+# - id
+# - add
+# - neg
+# - lt
+# - eq
+# - max
+# - is_close
+# - sigmoid
+# - relu
+# - log
+# - exp
+# - log_back
+# - inv
+# - inv_back
+# - relu_back
+#
+# For sigmoid calculate as:
+# $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
+# For is_close:
+# $f(x) = |x - y| < 1e-2$
+
+
+def mul(x: float, y: float) -> float:
+    return x * y
+
+
+def id(x: float) -> float:
+    return x
+
+
+def add(x: float, y: float) -> float:
+    return x + y
+
+
+def neg(x: float) -> float:
+    return -x
+
+
+def lt(x: float, y: float) -> float:
+    return 1 if x < y else 0
+
+
+def eq(x: float, y: float) -> float:
+    return 1 if x == y else 0
+
+
+def max(x: float, y: float) -> float:
+    return x if x > y else y
+
+
+def is_close(x: float, y: float) -> float:
+    return abs(x - y) < 1e-2
+
+
+def sigmoid(x: float) -> float:
+    return 1 / (1 + math.exp(-x)) if x >= 0 else math.exp(x) / (1 + math.exp(x))
+
+
+def relu(x: float) -> float:
+    return (x > 0) * x
+
+
+EPS = 1e-6
+
+
+def log(x: float) -> float:
+    return math.log(x + EPS)
+
+
+def exp(x: float) -> float:
+    return math.exp(x)
+
+
+def log_back(x: float, d: float) -> float:
+    return d / (x + EPS)
+
+
+def inv(x: float) -> float:
+    return 1 / (x + EPS)
+
+
+def inv_back(x: float, d: float) -> float:
+    return -d / (x * x)
+
+
+def relu_back(x: float, d: float) -> float:
+    return (x > 0) * d
+
+
+# ## Task 0.3
+
+# Small practice library of elementary higher-order functions.
+
+# Implement the following core functions
+# - map
+# - zipWith
+# - reduce
+#
+# Use these to implement
+# - negList : negate a list
+# - addLists : add two lists together
+# - sum: sum lists
+# - prod: take the product of lists
+
+
+# TODO: Implement for Task 0.3.
---
+++ run_tests.py
@@ -0,0 +1,9 @@
+# from minitorch.testing import MathTest, MathTestVariable
+
+# one_arg_tests = MathTestVariable._comp_testing()[0]
+# for test_name, test1, test2 in one_arg_tests:
+#     success = True
+#     for i in range(-100, 100):
+#         if test1(i) != test2(i):
+#             success = False
+#     print(f'{test_name}: {"PASS" if success else "FAIL"}')
